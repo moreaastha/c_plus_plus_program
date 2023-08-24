35dbf7197822c88b0d5f0c3280170e84f1f20857
moreaastha <142910190+moreaastha@users.noreply.github.com>
2023-08-24 20:30:04 +0530

    Add files via upload

---
+++ sample.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int a, b, c, x;
+    cout <<"Enter the values of a, b, c: ";
+    cin >> a >> b >> c;
+    x = a / b - c;
+    cout << x;
+    return 0;
+}
