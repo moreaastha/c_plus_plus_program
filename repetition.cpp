2804af1376717401a45e9b305aa939ced0b16d5d
Aastha More <142910190+moreaastha@users.noreply.github.com>
2023-08-25 20:23:52 +0530

    Add files via upload

---
+++ repetition.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int i, num;
+    cout << "Enter the number of repetition: " << endl;
+    cin >> num;
+    for(i = 0; i < num; i++)
+    {
+    cout << "Badhai hoo" << endl;
+    }
+    return 0;
+}
