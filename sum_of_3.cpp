c98d95b6d3d45063679f0cb98bad7cf17d202b06
Aastha More <142910190+moreaastha@users.noreply.github.com>
2023-08-27 13:35:28 +0530

    Add files via upload

---
+++ sum_of_3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<conio.h>
+using namespace std;
+int main() 
+{
+    int a,b,c,sum;
+    cout<<"Enter three values: "<<endl;
+    cin>>a>>b>>c;
+    sum = a + b + c;
+    cout<<"The sum of three values is: "<<sum;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
