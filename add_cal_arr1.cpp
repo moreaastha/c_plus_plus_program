2f082c2b0356fc6ffbf4eb0670ed1dfe7237c1b8
Aastha More <142910190+moreaastha@users.noreply.github.com>
2023-10-05 19:55:48 +0530

    Add files via upload

--- add_cal_arr1.cpp
+++ add_cal_arr1.cpp
@@ -2,16 +2,26 @@
 using namespace std;
 int main() 
 {
-    int BA, w, k, x, LB;
+    int BA, w, x, i, j, M, N, U1, L1, U2, L2;
     cout<<"Enter the base address: ";
     cin>>BA;
     cout<<"Enter the space of elements: ";
     cin>>w;
-    cout<<"Enter the address to be searched: ";
-    cin>>k;
-    cout<<"Enter the value of lower bound: ";
-    cin>>LB;
-    x = BA + w * (k - LB);
-    cout<<"The address of"<<"["<<k<<"]"<<"is: "<<x;
+    cout<<"Enter the lower bound: ";
+    cin>>L1;
+    cout<<"Enter the upper bound: ";
+    cin>>U1;
+    cout<<"Enter the lower bound 2: ";
+    cin>>L2;
+    cout<<"Enter the upper bound 2: ";
+    cin>>U2;
+    cout<<"Enter the value of i: ";
+    cin>>i;
+    cout<<"Enter the value of j: ";
+    cin>>j;
+    M = U1 - L1 +1;
+    N = U2 - L2 +1;
+    x = BA + w * (N * (i - L1) + (j - L2));
+    cout<<"The ans is: "<<x<<endl;
     return 0;
 }    
