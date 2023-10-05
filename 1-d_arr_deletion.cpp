b3a62cdd62084a36934a8d49bf9cea04de940700
Aastha More <142910190+moreaastha@users.noreply.github.com>
2023-10-05 20:10:42 +0530

    Update 1-d_arr_deletion.cpp

--- 1-d_arr_deletion.cpp
+++ 1-d_arr_deletion.cpp
@@ -2,36 +2,14 @@
 using namespace std;
 int main()
 {
-	int a[5];
-	int max=3;
-	int item;
-	cout<<"enter the value of item"<<endl;
-	cin>>item;
-	int loc;
-	cout<<"enter the value of loc"<<endl;
-	cin>>loc;
-	cout<<"enter the array element"<<endl;
-	for(int i=0;i<max;i++)
-	{
-		cin>>a[i];
-	}
-	int ub=max-1;
-	if(ub==5-1)
-	{
-	    cout<<"overflow condition"<<endl;
-	    
-	}
-	else
-	{
-	    for(int i=ub;i>=loc;i--)
-	    {
-	        a[i+1]=a[i];
-	    }
-	    a[loc]=item;
-	}
-	for(int i=0;i<5;i++)
-	{
-		cout<<a[i]<<" ";
+	int arr[5] = {1,2,3,4,5};
+	int max=5,pos;
+	cout<<"pos ";
+	cin>>pos;
+	arr[pos] = 0;
+    for(int i=0; i<max; i++)
+    {
+		cout<<arr[i]<<" ";
 	}
 
 	return 0;
