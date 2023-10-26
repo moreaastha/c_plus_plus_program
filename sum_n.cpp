26616cd24963542b6c186b2522484bac20214ffc
Aastha More <142910190+moreaastha@users.noreply.github.com>
2023-10-26 20:07:18 +0530

    Create sum_n.cpp

---
+++ sum_n.cpp
@@ -0,0 +1,11 @@
+ int sum = 0;
+    
+    int n;
+    cout<<"Enter value of n ";
+    cin>>n;
+
+    for(int i=0;i<=n;i++){
+        sum = sum+i;
+    }
+
+    cout<<"Sum till n of given No. is = "<<sum;
