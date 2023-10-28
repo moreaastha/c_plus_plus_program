41952f059ec23000a642878f83c04701c71b5977
Aastha More <142910190+moreaastha@users.noreply.github.com>
2023-10-28 19:47:59 +0530

    Create switch_statement.cpp

---
+++ switch_statement.cpp
@@ -0,0 +1,31 @@
+
+    char n;
+    cout<<"Enter any Lower Case Char only ";
+    cin>>n;
+
+    switch(n){
+
+        case 'a':
+            cout<<"Vowel";
+            break;
+
+        case 'e':
+            cout<<"Vowel";    
+            break;
+
+        case 'i':
+            cout<<"Vowel";    
+            break;
+
+        case 'o':
+            cout<<"Vowel";    
+            break;
+
+        case 'u':
+            cout<<"Vowel";    
+            break;
+
+        default:
+            cout<<"Consonant";
+
+    }
